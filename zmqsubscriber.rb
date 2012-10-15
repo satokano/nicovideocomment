aace0b299900011523e798217c9e2662adc71acc
Satoshi OKANO <satokano@gmail.com>
2012-10-15 19:07:42 +0900

    delete tag

--- zmqsubscriber.rb
+++ zmqsubscriber.rb
@@ -16,7 +16,7 @@ begin
   zmq_context = ZMQ::Context.new
   zmq_sock = zmq_context.socket(ZMQ::SUB)
   zmq_sock.connect("tcp://127.0.0.1:5000")
-  zmq_sock.setsockopt(ZMQ::SUBSCRIBE,"") # 全部受ける
+  zmq_sock.setsockopt(ZMQ::SUBSCRIBE, "") # 全部受ける
   
   # zmq_sockの操作の前後で使う
   #zmq_semaphore = Mutex.new
@@ -26,8 +26,9 @@ rescue => exception
   zmq_context = nil
 end
 
-while message = zmq_sock.recv
-  message.force_encoding("UTF-8")
+while tag_message = zmq_sock.recv
+  # tag_message.force_encoding("UTF-8") # 不要？
+  message = tag_message.scan(/allmsg (.+)/).first.first
   flat = JSON.parse(message)
   puts flat["text"]
 end
