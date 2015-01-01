0f10ea44e9d8c0e57b9d71d196167b0f6b85fec7
Satoshi OKANO <satokano@gmail.com>
2015-01-01 12:09:48 +0900

    cre

--- rmq_collector.rb
+++ rmq_collector.rb
@@ -260,7 +260,7 @@ class RmqCollector
       @rmqconn = MarchHare.connect(:host => @rmq_ip, :port => @rmq_port)
     rescue MarchHare::ConnectionRefused => cre
       puts "RabbitMQへのコネクション確立エラー: #{cre.to_s}\n"
-      puts rce.backtrace
+      puts cre.backtrace
       abort
     end
     # MarchHareではstartは意味ないらしい
