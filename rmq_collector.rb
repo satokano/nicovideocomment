1826f8fb937bb1d593ee32e2625cb4328950933c
Satoshi OKANO <satokano@gmail.com>
2015-01-01 12:08:00 +0900

    cre

--- rmq_collector.rb
+++ rmq_collector.rb
@@ -259,7 +259,7 @@ class RmqCollector
     begin
       @rmqconn = MarchHare.connect(:host => @rmq_ip, :port => @rmq_port)
     rescue MarchHare::ConnectionRefused => cre
-      puts "RabbitMQへのコネクション確立エラー: #{rce.to_s}\n"
+      puts "RabbitMQへのコネクション確立エラー: #{cre.to_s}\n"
       puts rce.backtrace
       abort
     end
