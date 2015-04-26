c4f95520cf6fbacb410829d8423edc3583c78d9d
Satoshi OKANO <satokano@gmail.com>
2015-04-26 23:22:34 +0900

    スタートアップスクリプト追加

---
+++ bin/startup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+fullpath=`readlink -f $0`
+binpath=`dirname $fullpath`
+cd $binpath/../
+NVC_HOME=`pwd`
+
+echo "NVC_HOME=${NVC_HOME}"
+
+echo "Start scraper....."
+cd ./scraper
+jruby -S bundle exec jruby ./scraper.rb 
+ret=$?
+
